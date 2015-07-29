afa1fcf246ec5247eb6755e3c274186c3cc43473
Torsten Werner <torsten.werner@westernacher.com>
2015-07-29 11:10:43 +0200

    initial import

---
+++ src/main/java/jsf/sample/ServletInitializer.java
@@ -0,0 +1,13 @@
+package jsf.sample;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TomahawkSampleApplication.class);
+	}
+
+}
---
+++ src/main/java/jsf/sample/TomahawkSampleApplication.java
@@ -0,0 +1,12 @@
+package jsf.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomahawkSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomahawkSampleApplication.class, args);
+    }
+}
