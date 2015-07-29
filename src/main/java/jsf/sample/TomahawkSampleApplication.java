5be5463dab18299e8720c5f28c39329919a5cd59
Torsten Werner <torsten.werner@westernacher.com>
2015-07-29 13:22:20 +0200

    UI rendering works but the validation stuff is not yet working

--- src/main/java/jsf/sample/TomahawkSampleApplication.java
+++ src/main/java/jsf/sample/TomahawkSampleApplication.java
@@ -1,10 +1,40 @@
 package jsf.sample;
 
+import com.sun.faces.config.ConfigureListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.ServletContextAware;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
 
 @SpringBootApplication
-public class TomahawkSampleApplication {
+public class TomahawkSampleApplication implements ServletContextAware {
+    @Bean
+    public FacesServlet facesServlet() {
+        return new FacesServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean facesServletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(facesServlet(), "*.jsf");
+        registrationBean.setName("FacesServlet");
+        registrationBean.setLoadOnStartup(1);
+        return registrationBean;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+        return new ServletListenerRegistrationBean<>(new ConfigureListener());
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TomahawkSampleApplication.class, args);
